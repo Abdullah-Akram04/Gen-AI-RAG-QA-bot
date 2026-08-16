5b392eec0099c3275561051a8bf0718b345ce6cf
Abdullah-Akram04 <ch.abdullahakram786@gmail.com>
2026-08-15 19:44:43 -0700

    Add LLM connection file and load LLM to query

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+
+def llm_model(model_id):
+
+    llm = ChatGroq(
+        model=model_id,
+        temperature=0.5,
+        max_tokens=256
+    )
+
+    return llm
+
+
+llm = llm_model("openai/gpt-oss-20b")
+
+response = llm.invoke(
+    "Explain Retrieval Augmented Generation in simple words."
+)
+
+print(response.content)
